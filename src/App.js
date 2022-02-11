952d05a31e3510d954b850bd66670fda0ac6247f
zaska-afk <zacharybalaska@gmail.com>
2022-02-11 15:59:34 -0500

    SCSS added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
+import HomePage from "./hompage.component";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
